4bbe904d39ebedcaa2d96e1d38bd19e12b90bf9e
André Meier <meier@louis.info>
2015-05-22 10:51:15 +0200

    fix scopes

--- index.js
+++ index.js
@@ -6,20 +6,44 @@ var walkSync = require('walk-sync');
 
 var port = 35729;
 
-var snippet = [
-  "<!-- livereload snippet -->",
-  "<script>document.write('<script src=\"http://'",
-  " + (location.host || 'localhost').split(':')[0]",
-  " + ':" + port + "/livereload.js?snipver=1\"><\\/script>')",
-  "</script>",
-  ""
-].join('\n');
+function InjectLivereload(inputTree, options) {
+  if (!(this instanceof InjectLivereload)) {
+    return new InjectLivereload(inputTree, options);
+  }
 
-var handleFile = function (srcDir, destDir, relativePath) {
+  this.enforceSingleInputTree = true;
+
+  CachingWriter.apply(this, arguments);
+}
+
+InjectLivereload.prototype = Object.create(CachingWriter.prototype);
+InjectLivereload.prototype.constructor = InjectLivereload;
+
+InjectLivereload.prototype.updateCache = function (srcDir, destDir) {
+  var self = this;
+
+  walkSync(srcDir).forEach(function (relativePath) {
+    if (relativePath.slice(-1) !== '/') {
+      self.handleFile(srcDir, destDir, relativePath);
+    }
+  });
+};
+
+InjectLivereload.prototype.handleFile = function (srcDir, destDir, relativePath) {
   var fullInputPath = path.join(srcDir, relativePath);
   var fullOutputPath = path.join(destDir, relativePath);
   var contents;
-  if (isHtml(relativePath)) {
+
+  var snippet = [
+    "<!-- livereload snippet -->",
+    "<script>document.write('<script src=\"http://'",
+    " + (location.host || 'localhost').split(':')[0]",
+    " + ':" + port + "/livereload.js?snipver=1\"><\\/script>')",
+    "</script>",
+    ""
+  ].join('\n');
+
+  if (path.extname(relativePath) === '.html') {
     contents = fs.readFileSync(fullInputPath, 'utf-8');
     contents = contents.replace(/<\/body>/, function (w) {
       return snippet + w;
@@ -27,31 +51,9 @@ var handleFile = function (srcDir, destDir, relativePath) {
   } else {
     contents = fs.readFileSync(fullInputPath);
   }
+
   mkdirp.sync(path.dirname(fullOutputPath));
   fs.writeFileSync(fullOutputPath, contents);
-};
-
-function InjectLivereload(inputTree, options) {
-  if (!(this instanceof InjectLivereload)) {
-    return new InjectLivereload(inputTree, options);
-  }
-  this.enforceSingleInputTree = true;
-  CachingWriter.apply(this, arguments);
-}
-
-var isHtml = function (filename) {
-  return filename.slice(-5) === '.html';
-};
-
-InjectLivereload.prototype = Object.create(CachingWriter.prototype);
-InjectLivereload.prototype.constructor = InjectLivereload;
-
-InjectLivereload.prototype.updateCache = function (srcDir, destDir) {
-  walkSync(srcDir).forEach(function (relativePath) {
-    if (relativePath.slice(-1) !== '/') {
-      handleFile(srcDir, destDir, relativePath);
-    }
-  });
 };
 
 module.exports = InjectLivereload;
