8a3c2b6a2508fdac48311e2f467f067e4e1637bb
stfsy <s.pfaffel@gmail.com>
2017-09-13 07:49:48 +0200

    chore: test if import is a function

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,10 +1,13 @@
 'use strict'
 
-const Module = require('../../lib/index')
+const BroccoliInjectLivereload = require('../../lib/index')
 
 const expect = require('chai').expect
 const resolve = require('path').resolve
 
 describe('BroccoliInjectLivereload', () => {
 
+    it('is a constructor', () => {
+        typeof BroccoliInjectLivereload == 'function'
+    })
 })
