522c740a31483ef4c8d1f2683f71b59d5bbcf5e9
stfsy <s.pfaffel@gmail.com>
2017-09-13 08:14:57 +0200

    test: test index.js

--- test/spec/index.js
+++ test/spec/index.js
@@ -5,9 +5,45 @@ const BroccoliInjectLivereload = require('../../lib/index')
 const expect = require('chai').expect
 const resolve = require('path').resolve
 
+const tag = require(resolve('lib/tag'))
+const Injector = require(resolve('lib/injector'))
+
 describe('BroccoliInjectLivereload', () => {
 
     it('is a constructor', () => {
         typeof BroccoliInjectLivereload == 'function'
+    })
+    
+    // calling processString with a fake context
+    describe('.processString', () => {
+
+        let mockModuleContext = null
+        let htmlString = null
+
+        beforeEach(() => {
+            mockModuleContext = {
+                _building: false,
+                _injector: new Injector('^index.html$', tag('123456'))
+            }
+            htmlString = '<html><head></head><body></body></html>'
+        })
+
+        it('returns content unchanged', () => {
+            mockModuleContext._building = true
+            const buffer = BroccoliInjectLivereload.prototype.processString.call(mockModuleContext, new Buffer(htmlString), 'index.html')
+            expect(buffer.toString('utf-8')).to.equal(htmlString)
+        })
+
+        it('adds the livereload tag', () => {
+            const buffer = BroccoliInjectLivereload.prototype.processString.call(mockModuleContext, new Buffer(htmlString), 'index.html')
+            expect(buffer.toString('utf-8')).not.to.equal(htmlString)
+            expect(buffer.toString('utf-8')).to.contain('livereload.js')
+            expect(buffer.toString('utf-8')).to.contain('123456')
+        })
+
+        it('does not add the livereload tag', () => {
+            const buffer = BroccoliInjectLivereload.prototype.processString.call(mockModuleContext, new Buffer(htmlString), 'dex.html')
+            expect(buffer.toString('utf-8')).to.equal(htmlString)            
+        })
     })
 })
