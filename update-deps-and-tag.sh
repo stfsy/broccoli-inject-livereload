1883de3aab60ea2d082a510f2f078e80a05691e3
stfsy <s.pfaffel@gmail.com>
2018-12-22 14:26:42 +0100

    chore: update update script

--- update-deps-and-tag.sh
+++ update-deps-and-tag.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+git checkout master
 npm outdated
 npm update
 npm test
 git add *package*
 git commit -m "feat: update dependencies"
-git push origin master
+npm run release-minor
+git checkout dev || true
+git rebase master
