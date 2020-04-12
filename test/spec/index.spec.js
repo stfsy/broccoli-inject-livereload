26534e612fa089741cea343c66451d47b1299570
stfsy <s.pfaffel@gmail.com>
2020-04-12 16:03:08 +0200

    refactor: refactor browser and page handling

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const BroccoliInjectLivereload = require('../../lib/index')
 const BroccoliTestRunner = require('broccoli-test-runner')
 const runner = new BroccoliTestRunner('test/fixtures')
 
@@ -9,39 +8,28 @@ const puppeteer = require('puppeteer')
 
 describe('BroccoliInjectLivereload', () => {
     let page = null
-
     before(() => {
         return runner.serve()
-    })
-    before(() => {
-        return puppeteer.launch({ headless: true }).then((b) => {
-            return b.newPage()
-        }).then((p) => {
-            page = p
-        })
+            .then(() => puppeteer.launch({ headless: true }))
+            .then(browser => browser.newPage())
+            .then(p => page = p)
     })
     after(() => {
-        return runner.stop().then(() => {
-            page.browser().close()
-        })
+        return runner.stop()
+            .then(() => page.browser().close())
     })
     it('should inject livereload tag into test.html', () => {
         return page.goto('http://localhost:4200/test.html')
             .then(() => {
-                return page.evaluate(() => {
-                    return document.querySelector("script").textContent;
-                })
+                return page.evaluate(() => document.querySelector("script").textContent)
             }).then((text) => {
                 expect(text).to.contain("livereload.js")
             })
     })
-
     it('should NOT inject livereload tag into index.html', () => {
         return page.goto('http://localhost:4200/index.html')
             .then(() => {
-                return page.evaluate(() => {
-                    return document.querySelector("script");
-                })
+                return page.evaluate(() => document.querySelector("script"))
             }).then((text) => {
                 expect(text).to.be.null
             })
